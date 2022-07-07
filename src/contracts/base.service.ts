7ec0bd2e728108d4f30ee398adc0687117e8e983
Mostafa Gholami <mostafa.gholami@avidability.com>
2022-07-07 15:13:28 +0430

    source updated

--- src/contracts/base.service.ts
+++ src/contracts/base.service.ts
@@ -1,10 +1,16 @@
 export class BaseService {
   _userId: string;
+  _organizationId: string;
+  _hasFullAccess: boolean = false;
   _user: any;
+  _organization: any;
 
   user(user: any) {
     this._userId = user.id;
+    this._organizationId = user.organization_id;
     this._user = user;
+    this._organization = user.organization;
+    this._hasFullAccess = Boolean(user.full_access);
     return this;
   }
 
