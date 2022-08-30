91d95c78ae5e071ae9182f7fbc2df3839a9d53ac
Mostafa Gholami <mostafa.gholami@avidability.com>
2022-08-30 16:03:38 +0430

    crypto fixed

--- src/crypto/crypto.service.ts
+++ src/crypto/crypto.service.ts
@@ -12,19 +12,25 @@ export type TextEncrypt = {
 export class CryptoService {
   constructor(@Inject(CRYPTO_SECRET) private readonly secret, @Inject(CRYPTO_ALGORITHM) private readonly algorithm) {}
 
-  encrypt(text): TextEncrypt {
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv(this.algorithm, this.secret, iv);
-    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+  encrypt(text: string): TextEncrypt {
+    const initVector = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(this.algorithm, this.secret, initVector);
+
+    let encrypted = cipher.update(text, 'utf-8', 'hex');
+    encrypted += cipher.final('hex');
+
     return {
-      iv: iv.toString('hex'),
-      content: encrypted.toString('hex'),
+      iv: initVector.toString('hex'),
+      content: encrypted,
     };
   }
 
   decrypt(hash: TextEncrypt) {
     const decipher = crypto.createDecipheriv(this.algorithm, this.secret, Buffer.from(hash.iv, 'hex'));
-    const decrypted = Buffer.concat([decipher.update(Buffer.from(hash.content, 'hex')), decipher.final()]);
-    return decrypted.toString();
+
+    let decrypted = decipher.update(hash.content, 'hex', 'utf-8');
+    decrypted += decipher.final('utf8');
+
+    return decrypted;
   }
 }
